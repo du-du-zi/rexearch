43ce2134f72c054571b211b2b6f2beb3d90ee1cf
du-du-zi <jaehee_sg@naver.com>
2022-05-12 17:22:21 +0900

    단위 숫자 추출 정규식 변경

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -8,7 +8,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 import rexearch  # noqa : E402
 
 rx = rexearch.Rexearch()
-rx.load_json_file("tests/unit.number.rule.json")
+rx.load_json_file("tests/test.rule.json")
+# rx.load_json_file("tests/unit.number.rule.json")
 # rx.load_json_file("tests/jay.test.rules.json")
 sample_input = open("tests/titles.csv", mode="r",encoding='utf-8')
 
@@ -17,7 +18,7 @@ rx.custom_functions["find_unit_number"] = unit_rule.find_unit_number
 
 rdr = csv.reader(sample_input)
 
-with open("output_.csv",'w',encoding='utf-8') as f:
+with open("output_rule3_top50.csv",'w',encoding='utf-8') as f:
     writer_object =  csv.writer(f)
 
     for i, line in enumerate(rdr):
@@ -27,6 +28,8 @@ with open("output_.csv",'w',encoding='utf-8') as f:
         for result in results:
             extract_.append("\'" + result['raw'] + "\'")
             repr_.append("\'" + result['repr'] + "\'")
+            # print(result['tags'])
+            print(result)
         if extract_:
             line.append("||||".join(extract_))
             line.append("||||".join(repr_))
@@ -34,10 +37,7 @@ with open("output_.csv",'w',encoding='utf-8') as f:
             line.append(None)
             line.append(None)
         writer_object.writerow(line)
+        if i == 50:
+            break
     f.close()
 sample_input.close()
-
-
-# results = rx.search(sample_input)
-# for result in results:
-#     print(result)
