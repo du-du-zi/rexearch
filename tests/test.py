218e1c3410c67a0bac9e122b58ee69a37427a694
du-du-zi <jaehee_sg@naver.com>
2022-05-26 14:57:37 +0900

    룰 수정

--- tests/test.py
+++ tests/test.py
@@ -5,13 +5,40 @@ hangle_numbers = {'.':['.',0],'점':['.',0], '영':[0,0],'공':[0,0],'일':[1,0]
 '십':[10,1],'백':[10**2,1],
 '천':[10**3,1],'만':[10**4,1],'억':[10**8,1],'조':[10**12,1]}
 
-# custom function
-def find_FE(price):
+def rep_num(price):
+    #뒤에서부터 단위 수로 쪼개기
     number_table = [hangle_numbers[token] for token in price if token in hangle_numbers ]
+    max_unit = 10
     
+    idxs = list()
+    
+    for i,item in enumerate(number_table[::-1]):
+        num = item[0]
+        unit = item[1]
+        if unit and num > max_unit:
+            max_unit = num
+            idxs.append(i)
+    prev = 0
+    res = 0
+    length = len(number_table)
+    for idx in idxs[::-1]:
+        next = length-idx
+        if number_table[prev:next] : res += find_FE(number_table[prev:next])
+        prev = next
+    if number_table[prev:] : res += find_FE(number_table[prev:])
+    
+    return res
+
+# custom function
+def find_FE(number_table):
+    
+    #단위 앞에 수가 생략된 경우 처리
+    if number_table[0][1]:
+        number_table.insert(0,[1,0])
+
     def is_unit(table_index):
         return number_table[table_index][1]
-    
+
     result = str(number_table[0][0])
 
     max_unit = 10
@@ -27,9 +54,12 @@ def find_FE(price):
             else:
                 result += "+"
         result += str(number_table[i][0])     
-    return format(eval(result),',') 
-    
-    
+    return eval(result) 
+sl = ['4만5000','4.5만','4만5천','45000','사만오천원','천삼백억','8억7000만원','8억7000원','팔백오십만원','구천팔백사십칠만육천삼백이십오','이천 이백만원']    
+# sl = ['4.5만']    
+for s in sl:
+    print(s,':', format(rep_num(s),','))
+
 # print(find_FE('8십억 육천만'))
 # print(find_FE('십억 6000만'))
 # print(find_FE('10억 육천만'))
@@ -38,6 +68,7 @@ def find_FE(price):
 # print(find_FE('십8만'))
 # print(find_FE('십팔만'))
 # print(find_FE('5억천3백'))
+# print(find_FE('5억일천3백만'))
 # print(find_FE('팔천오백만'))
 # print(find_FE('일공공공칠일'))
 # print(find_FE('백오'))
@@ -49,4 +80,6 @@ def find_FE(price):
 # print(find_FE('십이.공공구달러'))
 # print(find_FE('13점영3엔'))
 # print(find_FE('십3점7억'))
-print(find_FE('천200'))
+# print(find_FE('천200'))
+# print(find_FE('천이백만원'))
+# print(find_FE('일천 이백만'))
